698b110f62938760e813d1434908a4feb0adc0e0
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-09-16 20:50:35 +0700

    3.5. Sửa layout website

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ connectDatabase();
 import { routesClient } from "./routes/client/index.route";
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
-
+app.use(express.static("public"));
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 routesClient(app);
