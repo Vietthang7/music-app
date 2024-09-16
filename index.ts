01fc8d5e753271054dc34dfb619c2bca210ae628
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-09-16 18:33:08 +0700

    2.5. Lấy ra chủ đề bài hát

--- index.ts
+++ index.ts
@@ -3,13 +3,18 @@ import dotenv from "dotenv";
 dotenv.config();
 import { connectDatabase } from "./config/database";
 connectDatabase();
+import Topic from "./models/topic.model";
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
-app.get("/topics", (req: Request, res: Response) => {
+app.get("/topics", async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false
+  });
+  console.log(topics);
   res.render("client/pages/topics/index");
 });
 app.listen(port, () => {
