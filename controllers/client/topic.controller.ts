89f285df68f57038cc411dcf915d07eea4d87d06
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-09-16 22:06:34 +0700

    5. Làm trang danh sách bài hát (bên client)

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -5,7 +5,6 @@ export const index = async (req: Request, res: Response) => {
   const topics = await Topic.find({
     deleted: false
   });
-  console.log(topics);
   res.render("client/pages/topics/index", {
     pageTitle : "Danh sách chủ đề",
     topics : topics
---
+++ routes/client/song.route.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+import * as controller from "../../controllers/client/song.controller";
+
+
+router.get("/:slugTopic", controller.list);
+
+
+export const songRoute = router;
