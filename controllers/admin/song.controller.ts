485093266a8d4e5ba6dc0f3bab1936d06aca45ca
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-09-23 14:01:32 +0700

    4. Tạo trang Danh sách bài hát (bên admin)

---
+++ controllers/admin/song.controller.ts
@@ -0,0 +1,13 @@
+import { Request, response, Response } from "express";
+import Song from "../../models/song.model";
+
+//[GET]/admin/songs
+export const index = async (req: Request, res: Response) => {
+  const songs = await Song.find({
+    deleted: false
+  });
+  res.render("admin/pages/songs/index", {
+    pageTitle: "Quản lí bài hát",
+    songs: songs
+  });
+};
