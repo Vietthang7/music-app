f6cf60b34f3febcf67e57d6986dfc0f8a7e3e28c
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-09-26 18:53:54 +0700

    3. Trang Chỉnh sửa bài hát

--- routes/admin/song.route.ts
+++ routes/admin/song.route.ts
@@ -21,4 +21,19 @@ router.post("/create",
   uploadCloud.uploadFields,
   controller.createPost
 );
+router.get("/edit/:id", controller.edit);
+router.patch("/edit/:id",
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1
+    },
+    {
+      name: "audio",
+      maxCount: 1
+    }
+  ]),
+  uploadCloud.uploadFields,
+  controller.editPatch
+);
 export const songRoute = router;
